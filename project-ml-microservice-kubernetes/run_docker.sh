b0c66c557cca89febbf4b65a3c12950c4ef96b38
Akin <>
2022-07-28 13:14:12 +0100

    containerize the application

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=project-ml-microservice-kubernetes .
+
+docker build --tag=udacity_project4 .
+
 # Step 2: 
 # List docker images
 docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 project-ml-microservice-kubernetes
+docker run -p 8000:80 udacity_project4
