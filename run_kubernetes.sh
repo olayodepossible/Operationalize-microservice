fa8335dd09eaa885c8e3276970dd287ddb1a859c
Akin <>
2022-07-31 18:50:12 +0100

    refactor Makefile

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=opossible/udacity-project4:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment udacity-project4 --image=$dockerpath --port=80
+kubectl create deployment udacity-project4 --image=$dockerpath --port=80  --image-pull-policy='never'
 
 # Step 3:
 # List kubernetes pods
